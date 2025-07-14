dc9aa0f92368dae4288b98bf97a794cfad234bb5
Abdul Rafay <inshalnadeem01@gmail.com>
2025-07-14 09:21:49 +0500

    Hello Agent

---
+++ open-ai-agent-sdk/project-1/main.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI
+from agents.run import RunConfig
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError(
+        "GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are a helpful assistant.",
+    model=model,
+)
+
+result = Runner.run_sync(
+    agent,
+    "What is the capital of Pakistan?",
+    run_config=config,
+)
+
+
+def main():
+
+    print("Hello Agent-1!\n")
+
+    print("User Input:", result.input)
+    print("Result:", result.final_output)
+
+
+if __name__ == "__main__":
+    main()
